da01677879da2c9352e75541281578e96ed95baf
Rajibsigdel12345 <rajibsigdel12345@gmail.com>
2024-05-21 11:31:42 +0545

    update

--- Backend/ecommerce/settings.py
+++ Backend/ecommerce/settings.py
@@ -96,12 +96,12 @@ DATABASES = {
     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
