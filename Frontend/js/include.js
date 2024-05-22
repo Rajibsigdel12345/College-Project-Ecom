16f94c5871510bb7fd18c1ee477b07398bb4f664
Rajibsigdel12345 <rajibsigdel12345@gmail.com>
2024-05-22 13:05:00 +0545

    update

--- Frontend/js/include.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  let includes = $('[data-include]')
-  $.each(includes, function () {
-    let file ='templates/'+$(this).data('include') + '.html'
-    $(this).load(file)
-  })
-})
-
-
