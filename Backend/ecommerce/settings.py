a118be63891dd2700fd84379f5b9168378bf7ece
Rajibsigdel12345 <76434595+Rajibsigdel12345@users.noreply.github.com>
2024-07-23 10:26:48 +0545

    update settings

--- Backend/ecommerce/settings.py
+++ Backend/ecommerce/settings.py
@@ -20,7 +20,6 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
