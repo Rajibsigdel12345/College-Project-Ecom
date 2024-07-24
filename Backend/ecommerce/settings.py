b0b7536726b9572f4ba53d5730be5dd7e523fff9
Rajibsigdel12345 <76434595+Rajibsigdel12345@users.noreply.github.com>
2024-07-24 11:19:00 +0545

    update

--- Backend/ecommerce/settings.py
+++ Backend/ecommerce/settings.py
@@ -103,7 +103,7 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': Path(BASE_DIR, 'data','db.sqlite3')
     }
 }
 
